c92f62b8439ca0f827b213b785cd11a75cd5a17c
Alex <argentum.tm@gmail.com>
2019-08-21 21:55:47 +0300

    added form

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ app.use(express.static(`${__dirname}/../${PUBLIC_PATH}`));
 app.use(bodyParser.json());
 app.get('/',(req,res) => {
     res.sendFile(path.resolve(`${__dirname}/../${PUBLIC_PATH}`, INDEX_FILE));;
+});
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.json({message:'good'});
 })
 const  server = app.listen(PORT, ()=> {
     console.log('Server is running on port '+PORT);
--- src/components/Form.js
+++ src/components/Form.js
@@ -8,37 +8,74 @@ class Form extends Component {
             email:'',
             phone:''
         };
+        this.handleNameChange = this.handleNameChange.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handleSubmit(){
-        console.log('email value is', this.state.email);
+    handleSubmit(event){
+        event.preventDefault();
+
+        fetch('/',{ method:'POST', headers:{'content-type':'application/json'}, body: JSON.stringify(this.state)})
+            .then((res)=>{
+                if(res.ok){
+                    return res.json();
+                }
+            }).then(function (data) {
+                  console.log(data.message);
+              })
+
+                      this.setState({
+                          name:'',
+                          email:'',
+                          phone:''
+                      })
+    }
+    handleNameChange(event){
+        this.setState({name:event.target.value});
     }
     handleEmailChange(event){
-        console.log('email was changed',event.target.value);
         this.setState({email:event.target.value});
+    }
+    handlePhoneChange(event){
+        this.setState({phone:event.target.value});
     }
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="name"
-                    value={this.state.name}
-                    onChange={this.handleNameChange}
-                />
-                <input
-                    type="text"
-                    placeholder="email"
-                    value={this.state.email}
-                    onChange={this.handleEmailChange}
-                />
-                <input
-                    type="text"
-                    placeholder="phone"
-                    value={this.state.phone}
-                    onChange={this.handlePhoneChange}
-                />
+            <form onSubmit={this.handleSubmit} className="form">
+                <div className="container">
+                    <label>ваше имя:</label>
+                    <input
+                        type="text"
+                        placeholder=" "
+                        value={this.state.name}
+                        onChange={this.handleNameChange}
+                        required
+                        minLength="2"
+                    />
+                    <label>email:</label>
+                    <input
+                        type="text"
+                        placeholder=" "
+                        value={this.state.email}
+                        onChange={this.handleEmailChange}
+                        required
+                        pattern="^[a-z,A-Z,0-9]{1}[a-z,A-Z,0-9,.,_,-,&,?,!]*[a-z,A-Z,0-9]{1}@{1}[a-z,A-Z,0-9,-]+.{1}[a-z,A-Z,0-9,-]+$"
+                        minLength="5"
+                    />
+                    <label>телефон:</label>
+                    <input
+                        type="text"
+                        placeholder=" "
+                        value={this.state.phone}
+                        onChange={this.handlePhoneChange}
+                        required
+                        pattern="^\+{1}[0-9]+$"
+                        minLength="12"
+                        maxLength="12"
+                    />
+                    <button>Отправить</button>
+                </div>
             </form>
         );
     }
